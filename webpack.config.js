e77eb89f5fb9406d4ca5b36c597bf68d881b04d1
Ana-Maria Ungureanu <ana-maria.ungureanu@zaplabs.com>
2018-08-23 17:44:09 -0700

    lint webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
 // import {HtmlWebPackPlugin} from 'html-webpack-plugin'; why doesn't this work?!
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports =  {
+module.exports = {
   module: {
     rules: [
       {
-        test:/\.(js|jsx)$/,
-        exclude:/node_modules/,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      template: './src/index.html',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
-}
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
